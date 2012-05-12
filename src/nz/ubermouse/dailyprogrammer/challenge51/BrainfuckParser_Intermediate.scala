7510223f91bfce0605b71208b13b3fee11e9e2f0
Taylor <ubermouse894@gmail.com>
2012-05-13 00:52:12 +1200

    [BrainfuckParser_Intermediate.scala] Fixed issue causing StackOverflow

--- src/nz/ubermouse/dailyprogrammer/challenge51/BrainfuckParser_Intermediate.scala
+++ src/nz/ubermouse/dailyprogrammer/challenge51/BrainfuckParser_Intermediate.scala
@@ -10,10 +10,10 @@ package nz.ubermouse.dailyprogrammer.challenge51
 object BrainfuckParser_Intermediate {
 
   val MEMORY_SIZE            = 64000
-  val cells                  = List.fill(MEMORY_SIZE)(0)
+  var cells                  = Array.fill(MEMORY_SIZE)(0)
   val parserFunctionMappings = Map(">" -> ((ptr: Int) => ptr + 1),
                                    "<" -> ((ptr: Int) => ptr - 1),
-                                   "-" -> ((ptr: Int) => {cells(ptr) += 1; ptr}),
+                                   "-" -> ((ptr: Int) => {cells(ptr) + 1; ptr}),
                                    "+" -> ((ptr: Int) => {cells(ptr) -= 1; ptr}))
 
   def apply(code: String) = parse(code.replaceAll(" ", ""))
@@ -21,7 +21,7 @@ object BrainfuckParser_Intermediate {
   private def parse(code: String, ptr: Int = 0):Int = {
     if (code.length == 1) parserFunctionMappings(code)(ptr)
     else {
-      parse(code.substring(1), parse(code.substring(0)))
+      parse(code.substring(1), parse(code.substring(0, 1)))
     }
   }
 }
