3a2a3efe03bb70ff3effa03c8192d80afdebd652
Taylor <ubermouse894@gmail.com>
2012-05-12 23:31:48 +1200

    Initial commit with previously written code

---
+++ src/nz/ubermouse/dailyprogrammer/challenge50/tests/T9Spelling.scala
@@ -0,0 +1,48 @@
+package nz.ubermouse.dailyprogrammer.challenge50.tests
+
+import nz.ubermouse.dailyprogrammer.challenge50.T9Spelling_Difficult
+import org.scalatest.WordSpec
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: UberMouse
+ * Date: 5/10/12
+ * Time: 6:11 PM
+ */
+
+class T9Spelling extends WordSpec
+{
+
+  "The T9 spelling object" should
+    {
+      "convert a string of text to it's representation on a T9 Keypad" in
+        {
+          val testString = "hello world"
+          val result = "4433555 555666096667775553"
+          expect(result)
+          {
+            T9Spelling_Difficult.parse(testString)
+          }
+        }
+
+      "accept non alphabetical characters in the input" in
+        {
+          val testString = "sup son!11!"
+          val result = "777788707777666 66!11!"
+          expect(result)
+          {
+            T9Spelling_Difficult.parse(testString)
+          }
+        }
+      "insert a space between characters of the same number group" in
+        {
+          val testString = "ss aa def"
+          val result = "7777 777702 203 33 333"
+          expect(result)
+          {
+            T9Spelling_Difficult.parse(testString)
+          }
+        }
+    }
+
+}
