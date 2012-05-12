ff4cbc7cc8db00be4cc9239952c4cd335ceb56c7
Taylor <ubermouse894@gmail.com>
2012-05-13 00:55:39 +1200

    [BrainfuckParser_Intermediate.scala] Fixed issue causing pointer operations to fail

--- src/nz/ubermouse/dailyprogrammer/challenge51/BrainfuckParser_Intermediate.scala
+++ src/nz/ubermouse/dailyprogrammer/challenge51/BrainfuckParser_Intermediate.scala
@@ -13,7 +13,7 @@ object BrainfuckParser_Intermediate {
   var cells                  = Array.fill(MEMORY_SIZE)(0)
   val parserFunctionMappings = Map(">" -> ((ptr: Int) => ptr + 1),
                                    "<" -> ((ptr: Int) => ptr - 1),
-                                   "-" -> ((ptr: Int) => {cells(ptr) + 1; ptr}),
+                                   "-" -> ((ptr: Int) => {cells(ptr) += 1; ptr}),
                                    "+" -> ((ptr: Int) => {cells(ptr) -= 1; ptr}))
 
   def apply(code: String) = parse(code.replaceAll(" ", ""))
@@ -21,7 +21,7 @@ object BrainfuckParser_Intermediate {
   private def parse(code: String, ptr: Int = 0):Int = {
     if (code.length == 1) parserFunctionMappings(code)(ptr)
     else {
-      parse(code.substring(1), parse(code.substring(0, 1)))
+      parse(code.substring(1), parse(code.substring(0, 1), ptr))
     }
   }
 }
