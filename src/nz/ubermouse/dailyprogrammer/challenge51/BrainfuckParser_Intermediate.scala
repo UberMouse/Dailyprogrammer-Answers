a6de2014767f429fb0e28504efb2d16562db599e
Taylor <ubermouse894@gmail.com>
2012-05-13 20:11:31 +1200

    [BrainfuckParserSpec.scala] Changed parse to use an offset int instead of throwing away parsed characters to allow for command jumping Added a function to find all the jump points in the code

--- src/nz/ubermouse/dailyprogrammer/challenge51/BrainfuckParser_Intermediate.scala
+++ src/nz/ubermouse/dailyprogrammer/challenge51/BrainfuckParser_Intermediate.scala
@@ -1,5 +1,7 @@
 package nz.ubermouse.dailyprogrammer.challenge51
 
+import collection.immutable.HashMap
+
 /**
  * Created by IntelliJ IDEA.
  * User: UberMouse
@@ -13,20 +15,45 @@ object BrainfuckParser_Intermediate {
   var cells                  = Array.fill(MEMORY_SIZE)(0)
   val parserFunctionMappings = Map(">" -> ((ptr: Int) => ptr + 1),
                                    "<" -> ((ptr: Int) => ptr - 1),
-                                   "-" -> ((ptr: Int) => {cells(ptr) -= 1; ptr}),
-                                   "+" -> ((ptr: Int) => {cells(ptr) += 1; ptr}),
-                                   "." -> ((ptr: Int) => {print(String.valueOf(cells(ptr).asInstanceOf[Char])); ptr}),
-                                   "," -> ((ptr: Int) => {cells(ptr) = readChar().asInstanceOf[Int]; ptr}))
+                                   "-" -> ((ptr: Int) => {
+                                     cells(ptr) -= 1;
+                                     ptr
+                                   }),
+                                   "+" -> ((ptr: Int) => {
+                                     cells(ptr) += 1;
+                                     ptr
+                                   }),
+                                   "." -> ((ptr: Int) => {
+                                     print(String.valueOf(cells(ptr).asInstanceOf[Char]));
+                                     ptr
+                                   }),
+                                   "," -> ((ptr: Int) => {
+                                     cells(ptr) = readChar().asInstanceOf[Int];
+                                     ptr
+                                   }))
+  var jumps = new HashMap[(String, Int), Int]
+
+  def findJumps(code: String): HashMap[(String, Int), Int] = {
+    var map = new HashMap[(String, Int), Int]
+    for (i <- 0 until code.length) {
+      val char = code(i)
+      if(char == "[")
+        map += ("[", i) -> code.indexOf("[", i)
+      else if (char == "]")
+        map += ("]", i) -> map.last
+    }
+    map
+  }
 
   def apply(code: String) = {
+    jumps = findJumps(code)
     cells = Array.fill(MEMORY_SIZE)(0)
+    //println(code, code.length)
     parse(code.replaceAll(" ", ""))
   }
 
-  private def parse(code: String, ptr: Int = 0):Int = {
-    if (code.length == 1) parserFunctionMappings(code)(ptr)
-    else {
-      parse(code.substring(1), parse(code.substring(0, 1), ptr))
-    }
+  private def parse(code: String, offset: Int = 0, ptr: Int = 0): Int = {
+      if (code.length == 1 || code.length-1 == offset) parserFunctionMappings(String.valueOf(code.last))(ptr)
+      else parse(code, offset + 1, parse(code.substring(offset, offset + 1), 0, ptr))
   }
 }
